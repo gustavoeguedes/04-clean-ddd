72db061cb202f8a7653527b846e4ae943d0c83a2
gustavoeguedes <gustavoestevesguedes12@gmail.com>
2025-04-08 17:01:14 -0300

    feat: add dayjs dependency and enhance Answer and Question entities with additional getters and excerpt method

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -14,6 +14,27 @@ export class Answer extends Entity<AnswerProps> {
     get content() {
         return this.props.content
     }
+
+    get questionId() {
+        return this.props.questionId
+    }
+
+    get authorId() {
+        return this.props.authorId
+    }
+
+    get createdAt() {
+        return this.props.createdAt
+    }
+
+    get updatedAt() {
+        return this.props.updatedAt
+    }
+
+    get excerpt() {
+        return this.content.substring(0, 120).trim().concat('...')
+    }
+
     static create(props: Optional<AnswerProps, 'createdAt'>, id?: UniqueEntityId) {
             const answer = new Answer({
                 ...props,
--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -3,6 +3,7 @@ import { Slug } from "./value-objects/slug"
 import { Entity } from "../../core/entities/entity"
 import { UniqueEntityId } from "../../core/entities/unique-entity-id"
 import { Optional } from "../../core/types/optional"
+import dayjs from 'dayjs'
 
 interface QuestionProps {
     authorId: UniqueEntityId
@@ -15,6 +16,39 @@ interface QuestionProps {
 }
 
 export class Question extends Entity<QuestionProps> {
+
+    get authorId() {
+        return this.props.authorId
+    }
+
+    get bestAnswerId() {
+        return this.props.bestAnswerId
+    }
+
+    get content() {
+        return this.props.content
+    }
+
+    get title() {
+        return this.props.title
+    }
+
+    get slug() {
+        return this.props.slug
+    }
+
+    get createdAt() {
+        return this.props.createdAt
+    }
+
+    get updatedAt() {
+        return this.props.updatedAt
+    }
+
+    get isNew(): boolean {
+        return dayjs().diff(this.createdAt, 'days') <= 3
+    }
+
     static create(props: Optional<QuestionProps, 'createdAt'>, id?: UniqueEntityId) {
         const question = new Question({
             ...props,

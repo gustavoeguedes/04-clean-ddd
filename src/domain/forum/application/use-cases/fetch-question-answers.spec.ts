90fc8b984c901b958b3bcdcb05564791bc6341ac
gustavoeguedes <gustavoestevesguedes12@gmail.com>
2025-05-12 11:10:47 -0300

    feat: add find answers by questionId

---
+++ src/domain/forum/application/use-cases/fetch-question-answers.spec.ts
@@ -0,0 +1,49 @@
+import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers-repository'
+import { makeAnswer } from '../../../../../test/factories/make-answer'
+import { FetchQuestionAnswersUseCase } from './fetch-question-answers'
+import { UniqueEntityId } from '../../../../core/entities/unique-entity-id'
+
+let inMemoryAnswersRepository: InMemoryAnswersRepository
+let sut: FetchQuestionAnswersUseCase
+beforeEach(() => {
+  inMemoryAnswersRepository = new InMemoryAnswersRepository()
+  sut = new FetchQuestionAnswersUseCase(inMemoryAnswersRepository)
+})
+
+describe('Fetch Question Answers', async () => {
+  it('should be able to fetch question answers', async () => {
+    await inMemoryAnswersRepository.create(
+      makeAnswer({ questionId: new UniqueEntityId('question-1') }),
+    )
+    await inMemoryAnswersRepository.create(
+      makeAnswer({ questionId: new UniqueEntityId('question-2') }),
+    )
+    await inMemoryAnswersRepository.create(
+      makeAnswer({ questionId: new UniqueEntityId('question-3') }),
+    )
+
+    const { answers } = await sut.execute({
+      questionId: 'question-1',
+      page: 1,
+    })
+
+    expect(answers).toHaveLength(1)
+  })
+
+  it('should be able to fetch paginated question answers', async () => {
+    for (let i = 1; i <= 22; i++) {
+      await inMemoryAnswersRepository.create(
+        makeAnswer({
+          questionId: new UniqueEntityId('my-question-id'),
+        }),
+      )
+    }
+
+    const { answers } = await sut.execute({
+      page: 2,
+      questionId: 'my-question-id',
+    })
+
+    expect(answers).toHaveLength(2)
+  })
+})
--- test/repositories/in-memory-answers-repository.ts
+++ test/repositories/in-memory-answers-repository.ts
@@ -1,5 +1,6 @@
 import { AnswersRepository } from '@/domain/forum/application/repositories/answers-repository'
 import { Answer } from '@/domain/forum/enterprise/entities/answer'
+import { PaginationParams } from '../../src/core/repositories/pagination-params'
 
 export class InMemoryAnswersRepository implements AnswersRepository {
   public items: Answer[] = []
@@ -19,6 +20,14 @@ export class InMemoryAnswersRepository implements AnswersRepository {
     if (!answer) return null
 
     return answer
+  }
+
+  async findManyByQuestionId(questionId: string, { page }: PaginationParams) {
+    const answers = this.items
+      .filter((item) => item.questionId.toString() === questionId)
+      .slice((page - 1) * 20, page * 20)
+
+    return answers
   }
 
   async save(answer: Answer) {

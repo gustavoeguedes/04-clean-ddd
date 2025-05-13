d31bfb580806f4ecca13c5e455c2ba23d1d9f75b
gustavoeguedes <gustavoestevesguedes12@gmail.com>
2025-05-13 13:25:24 -0300

    fead: add test

--- src/domain/forum/application/use-cases/delete-question-comment.spec.ts
+++ src/domain/forum/application/use-cases/delete-question-comment.spec.ts
@@ -1,6 +1,7 @@
 import { InMemoryQuestionCommentsRepository } from '../../../../../test/repositories/in-memory-question-comments-repository'
 import { DeleteQuestionCommentUseCase } from './delete-question-comment'
 import { makeQuestionComment } from '../../../../../test/factories/make-question-comment'
+import { UniqueEntityId } from '../../../../core/entities/unique-entity-id'
 
 let inMemoryQuestionCommentsRepository: InMemoryQuestionCommentsRepository
 let sut: DeleteQuestionCommentUseCase
@@ -23,5 +24,20 @@ describe('Delete Question Comment', async () => {
     })
 
     expect(inMemoryQuestionCommentsRepository.items).toHaveLength(0)
+  })
+
+  it('should not be able to delete another user question comment', async () => {
+    const questionComment = makeQuestionComment({
+      authorId: new UniqueEntityId('author-1'),
+    })
+
+    await inMemoryQuestionCommentsRepository.create(questionComment)
+
+    await expect(() => {
+      return sut.execute({
+        authorId: 'author-2',
+        questionCommentId: questionComment.id.toString(),
+      })
+    }).rejects.toBeInstanceOf(Error)
   })
 })

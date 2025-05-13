7b27164b596089d45e8c7206165a40b3e2a44b62
gustavoeguedes <gustavoestevesguedes12@gmail.com>
2025-05-13 16:53:20 -0300

    feat: add functional error handler

---
+++ src/core/either.spec.ts
@@ -0,0 +1,20 @@
+import { Either, left, right } from './either'
+
+function doSomething(shouldSuccess: boolean): Either<string, number> {
+  if (shouldSuccess) {
+    return right(10)
+  }
+  return left('error')
+}
+
+test('success result', () => {
+  const result = doSomething(true)
+
+  expect(result.isRight()).toBe(true)
+})
+
+test('error result', () => {
+  const result = doSomething(false)
+
+  expect(result.isRight()).toBe(true)
+})
